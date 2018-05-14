3213be3a13eed612183850d18a27322e1ede3e5f
mr_penumbra <mr_penumbra@Ravnen-PC>
2018-05-14 10:56:31 -0700

    Added Netbeans project file with blank main class. Will update the license later.

---
+++ src/tonerislow/TonerIsLow.java
@@ -0,0 +1,23 @@
+/*
+ * This code was written by Alec Ewers, whether he's proud of it
+ * or not. It is distributed freely so long as you include this
+ * header and give what you think is appropriate credit.
+ * This code comes with no warranty, implied or otherwise.
+ */
+
+package tonerislow;
+
+/**
+ *
+ * @author alec.ewers
+ */
+public class TonerIsLow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
