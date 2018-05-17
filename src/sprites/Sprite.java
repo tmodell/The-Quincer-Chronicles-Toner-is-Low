83a180cae61c590660dd1964a05e7cc1f3e90b43
albert.wilcox <albert.wilcox@OA-IA2-23.rjuhsd.int>
2018-05-17 10:07:51 -0700

    added methods and fields to the sprite class

--- src/sprites/Sprite.java
+++ src/sprites/Sprite.java
@@ -5,10 +5,47 @@
  */
 package sprites;
 
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author albert.wilcox
  */
 public abstract class Sprite {
+    int x;
+    int y;
+    //int realX;
+    //int realY;
+    Image image;
     
+    static final int GRID_SIZE = 100; // TODO add grid size info
+    
+    public Sprite(String url){
+        ImageIcon ii = new ImageIcon(url);
+        image = ii.getImage();
+    }
+    
+    public int getRealX(){
+        int realX = x * GRID_SIZE;
+        return realX;
+    }
+    
+    public int getRealY(){
+        int realY = y * GRID_SIZE;
+        realY -= image.getHeight(null);
+        return realY;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+    
+    public Image getImage(){
+        return image;
+    }
 }
