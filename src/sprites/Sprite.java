3656a31c3497c1a51d2fe9f36dfac6bf70b7a11a
albert.wilcox <albert.wilcox@OA-IA2-23.rjuhsd.int>
2018-05-17 11:06:14 -0700

    added methods and fields to the sprite class

--- src/sprites/Sprite.java
+++ src/sprites/Sprite.java
@@ -9,7 +9,7 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 /**
- *
+ * Abstract class encompassing each sprite
  * @author albert.wilcox
  */
 public abstract class Sprite {
@@ -26,11 +26,19 @@ public abstract class Sprite {
         image = ii.getImage();
     }
     
+    /**
+     * Finds the real X position to be used when drawing
+     * @return real X position
+     */
     public int getRealX(){
         int realX = x * GRID_SIZE;
         return realX;
     }
     
+    /**
+     * Find the real Y position to be used when drawing
+     * @return the real Y position
+     */
     public int getRealY(){
         int realY = y * GRID_SIZE;
         realY -= image.getHeight(null);
