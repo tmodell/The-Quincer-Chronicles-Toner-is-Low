65f8559224cb23f77eaa361e241edba07b71ff37
albert.wilcox <albert.wilcox@OA-IA2-23.rjuhsd.int>
2018-05-17 11:06:07 -0700

    added methods and fields to the sprite class

--- src/sprites/Player.java
+++ src/sprites/Player.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sprites;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  *
  * @author albert.wilcox
  */
-public class Player {
+public class Player extends Sprite implements KeyListener{
+    static final String PLAYER_IMAGE_URL = "TODO add url";
     
+    //TODO add reference to world
+    
+    public Player(int x, int y){
+        super(PLAYER_IMAGE_URL);
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {}
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        int key = ke.getKeyCode();
+        switch (key){
+            case KeyEvent.VK_LEFT:
+                x -= 1;
+                break;
+            case KeyEvent.VK_RIGHT:
+                //code
+                break;
+            case KeyEvent.VK_UP:
+                //code
+                break;
+            case KeyEvent.VK_DOWN:
+                //code
+                break;
+            
+               
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        
+    }
 }
