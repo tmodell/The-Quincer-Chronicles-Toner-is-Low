73d6f48d634c8da58049e501fec00b601b5cf0cd
alber <alber@192.168.1.136>
2018-05-18 16:52:31 -0700

    made movable, a parent class for sprites that can move. also added some wormer ai (its very primitive and sucks)

--- src/sprites/Player.java
+++ src/sprites/Player.java
@@ -7,15 +7,8 @@ import world.World;
  *
  * @author albert.wilcox
  */
-public class Player extends Sprite{
+public class Player extends Movable{
     static final String[] PLAYER_IMAGE_URLS = {"down facing url", "up facing url", "left facing url", "right facing url"};
-    
-    public static final int ORIENTATION_DOWN = 0;
-    public static final int ORIENTATION_UP = 1;
-    public static final int ORIENTATION_LEFT = 2;
-    public static final int ORIENTATION_RIGHT = 3;
-    
-    int orientation = 0;
     
     World world;
     
@@ -27,30 +20,30 @@ public class Player extends Sprite{
         this.world = world;
     }
 
+    @Override
     public void left(){
-        x -= 1;
-        orientation = ORIENTATION_LEFT;
+        super.left();
         ImageIcon ii = new ImageIcon(PLAYER_IMAGE_URLS[orientation]);
         image = ii.getImage();
     }
     
+    @Override
     public void right(){
-        x += 1;
-        orientation = ORIENTATION_RIGHT;
+        super.right();
         ImageIcon ii = new ImageIcon(PLAYER_IMAGE_URLS[orientation]);
         image = ii.getImage();
     }
     
+    @Override
     public void down(){
-        y += 1;
-        orientation = ORIENTATION_DOWN;
+        super.down();
         ImageIcon ii = new ImageIcon(PLAYER_IMAGE_URLS[orientation]);
         image = ii.getImage();
     }
     
+    @Override
     public void up(){
-        y -= 1;
-        orientation = ORIENTATION_UP;
+        super.up();
         ImageIcon ii = new ImageIcon(PLAYER_IMAGE_URLS[orientation]);
         image = ii.getImage();
     }
--- src/sprites/Wormer.java
+++ src/sprites/Wormer.java
@@ -11,10 +11,12 @@ import world.*;
  *
  * @author albert.wilcox
  */
-public class Wormer extends Sprite{
+public class Wormer extends Movable{
     static final String WORMER_IMAGE_URL = "insert image url here";
     
     MainFrame frame;
+    Player player;
+    
     private volatile boolean alive = true;
     
     WormerThread thread;
@@ -38,10 +40,50 @@ public class Wormer extends Sprite{
             this.wormer = wormer;
         }
         
+        /**
+         * Move towards the player and return whether the wormer should attack.
+         * @return Whether or not the wormer needs to attack
+         */
+        public boolean moveTowardsPlayer(){
+            // Find distance between player and wormer
+            int dx = x - player.getX();
+            int dy = y - player.getY();
+            int distance = (int)(Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)));
+            
+            if (distance > 5) return false;
+            if (distance == 1) return true;
+            
+            //TODO this code has no way of handling obstacles
+            if (dx > dy){
+                if (dx > 0) left();
+                else right();
+            } else{
+                if (dy > 0) up();
+                else down();
+            }
+            
+            frame.updateWorld();
+            return false;
+        }
+        
+        /**
+         * This method will attack the user
+         */
+        public void attack(){
+            // todo attack logic
+        }
+        
+        // TODO this wormer ai needs alot of work
         @Override
         public void run(){
             while(alive){
-                
+                boolean attack = moveTowardsPlayer();
+                if (attack) {
+                    attack();
+                }
+                try{
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {/*lazy exception handling*/}
             }
         }
     }
