f2b767ef498677d712c454c1dd573c8a1e958028
alber <alber@192.168.1.136>
2018-05-18 15:43:16 -0700

    added support for multithreading; each wormer is its own thread

--- src/sprites/Wormer.java
+++ src/sprites/Wormer.java
@@ -5,10 +5,44 @@
  */
 package sprites;
 
+
+import world.*;
 /**
  *
  * @author albert.wilcox
  */
-public class Wormer {
+public class Wormer extends Sprite{
+    static final String WORMER_IMAGE_URL = "insert image url here";
     
+    MainFrame frame;
+    private volatile boolean alive = true;
+    
+    WormerThread thread;
+    
+    public Wormer(MainFrame frame){
+        super("WORMER_IMAGE_URL");
+        this.frame = frame;
+        
+        thread = new WormerThread(this);
+        thread.start();
+    }
+    
+    public void kill(){
+        alive = false;
+    }
+
+    private class WormerThread extends Thread{
+        Wormer wormer;
+        
+        WormerThread(Wormer wormer){
+            this.wormer = wormer;
+        }
+        
+        @Override
+        public void run(){
+            while(alive){
+                
+            }
+        }
+    }
 }
