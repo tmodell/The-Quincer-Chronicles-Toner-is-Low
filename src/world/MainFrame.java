28ee031f81cfed539ff6d03c31da6f1c5b7dd183
alber <alber@192.168.1.136>
2018-05-17 22:12:08 -0700

    this is now the JFrame that everything will be in

--- src/world/MainFrame.java
+++ src/world/MainFrame.java
@@ -16,28 +16,35 @@ import java.awt.event.KeyListener;
  */
 public class MainFrame extends JFrame implements KeyListener{
     
+    World world;
+    
     public MainFrame(){
         super();
+        
+        world = new World();
+        //TODO create objects for toolbar, dialogue box and stat bar
+        
         init();
     }
 
     public void init(){
+        //world = new World();
+        world.setFocusable(true);
+        world.addKeyListener(this);
+        getContentPane().add(world);
+        
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setUndecorated(true);
         setResizable(false);
-        setFocusable(true);
-        getContentPane().setBackground(new Color(204, 255, 0));
+        //setFocusable(true);
+        //getContentPane().setBackground(new Color(204, 255, 0));
         setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        addKeyListener(this);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
     @Override
-    public void keyTyped(KeyEvent e) {
-        System.out.println("Hey");
-        dispose();
-    }
+    public void keyTyped(KeyEvent e) {}
 
     @Override
     public void keyPressed(KeyEvent e) {
