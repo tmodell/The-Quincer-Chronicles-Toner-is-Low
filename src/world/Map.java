54eaafcdad0c9ae137c0d7e14ee1957e8000c432
jeevanjot.bhullar <jeevanjot.bhullar@OA-IA2-16.rjuhsd.int>
2018-05-18 09:08:25 -0700

    Minor Typo Correction

--- src/world/Map.java
+++ src/world/Map.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package world;
 
-/**
- *
- * @author Jacob
- */
+package world;
 
 import java.awt.Image;
 import java.util.ArrayList;
