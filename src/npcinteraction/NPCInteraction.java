a2407892bdff0932fe9cb551abcef241112cc439
alber <alber@192.168.1.136>
2018-05-17 20:30:55 -0700

    updated file names

---
+++ src/npcinteraction/NPCInteraction.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package npcinteraction;
+
+/**
+ *
+ * @author john.geronimo
+ */
+public class NPCInteraction {
+//Make it like a visual novel ( ͡° ͜ʖ ͡°)
+}
