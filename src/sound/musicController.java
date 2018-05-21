c9fd3ddac551f16ce28bfe09ace3eedc065cc362
jeevanjot.bhullar <jeevanjot.bhullar@OA-IA2-16.rjuhsd.int>
2018-05-21 09:40:55 -0700

    Updated Audio adds support for sound effects
    Changed organisational structure
    functional start/stop/resume

--- src/sound/musicController.java
+++ src/sound/musicController.java
@@ -7,39 +7,40 @@ import java.io.IOException;
 import java.net.*;
 import javax.sound.sampled.*;
 
-public class soundController {
+public class musicController {
     
     public File Sound;
     public Clip clip;
     
-    public void changeAudio(File playMe) throws MalformedURLException, UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
+    public void changeMusic(File playMe) throws MalformedURLException, UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
         clip = AudioSystem.getClip();
         clip.open(AudioSystem.getAudioInputStream(playMe));
-        clip.start();
+        clip.start();//Play Audio
         //Thread.sleep(clip.getMicrosecondLength()); //Only one at a time
     }
     
     public void startAudio() {
-        clip.start();
+        clip.start(); //Play/Resume Audio
     }
     
     public void stopAudio() {
-        clip.stop();
+        clip.stop(); //Pause Audio
     }
     
     public void trackOne() throws UnsupportedAudioFileException, LineUnavailableException, IOException, MalformedURLException, InterruptedException {
-         Sound = new File("./src/Sound/trackOne.wav");
-         changeAudio(Sound);
+        Sound = new File("./src/Sound/Music/trackOne.wav");
+        changeMusic(Sound);
     }
     
     public void trackTwo() throws UnsupportedAudioFileException, LineUnavailableException, IOException, MalformedURLException, InterruptedException {
-         Sound = new File("./src/Sound/trackTwo.wav");
-         changeAudio(Sound);
+        Sound = new File("./src/Sound/Music/trackTwo.wav");
+        changeMusic(Sound);
     }
     
     public void trackThree() throws UnsupportedAudioFileException, LineUnavailableException, IOException, MalformedURLException, InterruptedException {
-         Sound = new File("./src/Sound/trackThree.wav");
-         changeAudio(Sound);
+        Sound = new File("./src/Sound/Music/trackThree.wav");
+        changeMusic(Sound);
     }
     
+    //https://stackoverflow.com/questions/557903/how-can-i-wait-for-a-java-sound-clip-to-finish-playing-back
 }
