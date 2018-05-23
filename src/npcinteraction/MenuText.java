89ea91847583f6a529a2226c3b2bbd91a5aefa51
alec.ewers <mr_penumbra@Ravnen-PC>
2018-05-23 10:27:54 -0700

    added a whole bunch of stuff from the test area

--- src/npcinteraction/MenuText.java
+++ src/npcinteraction/MenuText.java
@@ -7,10 +7,104 @@
 
 package npcinteraction;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 /**
  *
  * @author alec.ewers
  */
 public class MenuText {
 
+    //This section is the reader, which imports the content of the file and
+    //passes it as separate lines out.
+    public static String [] reader (String target, int iteration) 
+            throws IOException {
+        ArrayList<String> parts = new ArrayList<>();
+        if (iteration == 0) {
+            String content = new String(Files.readAllBytes(Paths.get(target 
+                    + ".txt")));
+            for (String I: content.split("\n")) {
+                parts.add(I);
+            }
+        }
+        return parts.toArray(new String[parts.size()]);
+    }
+    
+    //This section includes the control booleans and the data values that must
+    //be kept consistent, plus all the get and set methods.
+    public static boolean getMenu() {
+        return isMenu;
+    }
+    
+    public static void setMenu(boolean newMenu) {
+        isMenu = newMenu;
+    }
+    
+    public static boolean getSpeaking() {
+        return isSpeaking;
+    }
+    
+    public static void setSpeaking (boolean newTalk) {
+        isSpeaking = newTalk;
+    }
+    
+    public static boolean getNext() {
+        return isNext;
+    }
+    
+    public static void setNext (boolean newFollow) {
+        isNext = newFollow;
+    }
+    
+    public static void setCursPos(int y) {
+        if (cursPos + y != -1 && cursPos + y != maxPos) {
+            cursPos += y;
+        }
+    }
+    
+    public static int getCursPos() {
+        return cursPos;
+    }
+    
+    public static void setMaxPos(int y) {
+        maxPos = y;
+    }
+    
+    public static int getMaxPos() {
+        return maxPos;
+    }
+    
+    public static String getOutputs(int index) {
+        return outputs.get(index);
+    }
+    
+    public static void setSpeechPos(int y) {
+        speechPos = y;
+    }
+    
+    public static int getSpeechPos() {
+        return speechPos;
+    }
+    
+    public static void setIteration(int y) {
+        iteration = y;
+    }
+    
+    public static int getIteration() {
+        return iteration;
+    }
+    
+    private static ArrayList<String> outputs = new ArrayList<>(); 
+    private static boolean isMenu = false;
+    private static boolean isNext = false;
+    private static boolean isSpeaking = false;
+    private static int cursPos = 0;
+    private static int maxPos = 0;
+    private static int speechPos = 0;
+    private static int iteration = 0;
+
+    
 }
