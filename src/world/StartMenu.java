5db69e6d584491fcb3055f3f746012470d942e05
albert.wilcox <albert.wilcox@OA-IA2-20.rjuhsd.int>
2018-05-23 11:12:05 -0700

    minor change

---
+++ src/world/StartMenu.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package world;
+
+import java.awt.Dimension;
+import javax.swing.*;
+
+/**
+ *  
+ * @author albert.wilcox
+ */
+public class StartMenu extends JPanel{
+    JButton start, exit;
+    JLabel header;
+    public StartMenu(){
+        setPreferredSize(new Dimension(1920, 1080));
+        
+    }
+}
