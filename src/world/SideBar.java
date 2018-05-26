20c3e7d3572b3ad1f40bf5eb66a7849c297f23b5
alber <alber@192.168.1.136>
2018-05-25 19:00:52 -0700

    minor hud changes

--- src/world/SideBar.java
+++ src/world/SideBar.java
@@ -7,11 +7,27 @@ import javax.swing.*;
  * @author alber
  */
 public class SideBar extends JPanel{
+    private static final String IMAGE_URL = "src/world/lib/sidebar.png";
+    
     MainFrame frame;
     
+    Image background;
+    
     public SideBar (MainFrame frame){
+        super();
+        
         this.frame = frame;
         setPreferredSize(new Dimension(256, 1080));
         setBackground(Color.BLUE);
+        
+        background = new ImageIcon(IMAGE_URL).getImage();
+        //repaint();
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        
+        g.drawImage(background, 0, 0, null);
     }
 }
